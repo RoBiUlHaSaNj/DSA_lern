8409f85cf3b5417e32b67ab5c34a00f54b544870
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-09-18 08:35:37 +0600

    lower .cpp

--- Binary search/lower .cpp
+++ Binary search/lower .cpp
@@ -1,24 +1,76 @@
-#include <bits/stdc++.h>
-using namespace std;
+//use vector
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main() {
-    int arr[] = {10, 90, 40, 90, 30, 50};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 90;
+    std::vector<int> v = {1, 3, 3, 5, 7};
 
-    // Sort the array before performing binary search
-    sort(arr, arr + n);
+    int value = 3;
+    auto it = std::lower_bound(v.begin(), v.end(), value);
 
-    // Use binary_search() to check if x is in the array
-    bool found = binary_search(arr, arr + n, x);
-
-    if (found) {
-        // Use lower_bound() to find the index of the first occurrence of x
-        int index = lower_bound(arr, arr + n, x) - arr;
-        cout << "Element " << x << " found at index: " << index << endl;
+    if (it != v.end()) {
+        std::cout << "Lower bound for " << value << " is at position: " << (it - v.begin()) << '\n';
     } else {
-        cout << "Element " << x << " not found in the array" << endl;
+        std::cout << "Value not found in range.\n";
     }
 
     return 0;
 }
+
+
+//use  bs
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int n = 5, x = 9;
+    int ind = lowerBound(arr, n, x);
+    cout << "The lower bound is the index: " << ind << "\n";
+    return 0;
+}
+//use ls
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> arr, int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= x) {
+            // lower bound found:
+            return i;
+        }
+    }
+    return n;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int n = 5, x = 15;
+    int ind = lowerBound(arr, n, x);
+    cout << "The lower bound is the index: " << ind << "\n";
+    return 0;
+}
+
