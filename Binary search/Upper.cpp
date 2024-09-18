4ca9d6ac94a4cc447cd85ee3c75d02d3b1035c9d
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-09-18 08:37:06 +0600

    Upper.cpp

---
+++ Binary search/Upper.cpp
@@ -0,0 +1,75 @@
+//ls
+#include <bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int> &arr, int x, int n) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > x) {
+            // upper bound found:
+            return i;
+        }
+    }
+    return n;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 9, 15, 19};
+    int n = 6, x = 9;
+    int ind = upperBound(arr, x, n);
+    cout << "The upper bound is the index: " << ind << "\n";
+    return 0;
+}
+
+//use bs
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 9, 15, 19};
+    int n = 6, x = 9;
+    int ind = upperBound(arr, x, n);
+    cout << "The upper bound is the index: " << ind << "\n";
+    return 0;
+}
+
+//use vector
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> v = {1, 3, 3, 5, 7};
+
+    int value = 3;
+    auto it = std::upper_bound(v.begin(), v.end(), value);
+
+    if (it != v.end()) {
+        std::cout << "Upper bound for " << value << " is at position: " << (it - v.begin()) << '\n';
+    } else {
+        std::cout << "Value not found in range.\n";
+    }
+
+    return 0;
+}
