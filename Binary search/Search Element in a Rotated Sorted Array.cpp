16197a43c9f93ac215daedc5ec244c70cf2fb05f
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-09-19 20:55:03 +0600

    Day-5

--- Binary search/Search Element in a Rotated Sorted Array.cpp
+++ Binary search/Search Element in a Rotated Sorted Array.cpp
@@ -5,6 +5,7 @@ int searchInRotatedArray(int arr[], int n, int k) {
     int low = 0, high = n - 1;
 
     while (low <= high) {
+        
         int mid = low + (high - low) / 2;
 
         // If the target is found at the mid index
