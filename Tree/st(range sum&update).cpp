9e91439bf64958babf7b78e5483a5ecf16c39a15
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-10-07 10:24:03 +0600

    Update st(range sum&update).cpp

--- Tree/st(range sum&update).cpp
+++ Tree/st(range sum&update).cpp
@@ -1,79 +1,110 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+//range sum
+// update value
+//max
+//min
 
-vector<int> tree;
-vector<int> arr;
+vector<ll>tree;
+vector<ll>arr;
 
-// Function to build the segment tree
-void build(int node, int start, int end) {
-    if (start == end) {
-        // Leaf node will have a single element
-        tree[node] = arr[start];
-    } else {
-        int mid = (start + end) / 2;
-        // Recursively build the segment tree
-        build(2 * node + 1, start, mid);
-        build(2 * node + 2, mid + 1, end);
-        // Internal node will have the sum of both of its children
-        tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+void biuld(ll node,ll st, ll ed)
+{
+    if(st==ed)
+    {
+        tree[node]=arr[st];
+    }
+    else
+    {
+
+        ll mid =(st+ed)/2;
+        biuld(2 * node + 1,st,mid );
+        biuld(2 * node + 2,mid+1,ed);
+        tree[node]=min(tree[2*node+1],tree[2*node+2]);
+         //tree[node]=tree[2*node+1]+tree[2*node+2]);
+               //tree[node]=max(tree[2*node+1],tree[2*node+2]);
     }
 }
 
-// Function to query the sum in the range [L, R]
-int query(int node, int start, int end, int L, int R) {
-    if (R < start || end < L) {
-        // range represented by a node is completely outside the given range
-        return 0; // Identity for sum
-    }
-    if (L <= start && end <= R) {
-        // range represented by a node is completely inside the given range
-        return tree[node];
-    }
-    // range represented by a node is partially inside and partially outside the given range
-    int mid = (start + end) / 2;
-    int leftSum = query(2 * node + 1, start, mid, L, R);
-    int rightSum = query(2 * node + 2, mid + 1, end, L, R);
-    return leftSum + rightSum; // Combine the results
-}
+void update(ll node,ll st,ll ed,ll idx,ll val)
+{
 
-// Function to update the value at index idx
-void update(int node, int start, int end, int idx, int val) {
-    if (start == end) {
-        // Leaf node
-        arr[idx] = val; // Update the value in the original array
-        tree[node] = val; // Update the tree
-    } else {
-        int mid = (start + end) / 2;
-        if (start <= idx && idx <= mid) {
-            // If idx is in the left child
-            update(2 * node + 1, start, mid, idx, val);
-        } else {
-            // If idx is in the right child
-            update(2 * node + 2, mid + 1, end, idx, val);
+    if(st==ed)
+    {
+        arr[idx]=val;
+        tree[node]=val;
+
+    }
+    else
+    {
+
+        ll mid = (st+ed)/2;
+        if(st<=idx && idx <=mid)//left
+        {
+            update(2*node+1,st,mid,idx,val);
         }
-        // Internal node will have the sum of both of its children
-        tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        else{
+            update(2*node+2,mid+1,ed,idx,val);
+        }
+         //tree[node]=tree[2*node+1]+tree[2*node+2];
+         tree[node]=min(tree[2*node+1],tree[2*node+2]);
+         //tree[node]=max(tree[2*node+1],tree[2*node+2]);
     }
 }
 
+ll querry(ll node,ll st, ll ed,ll L,ll R)
+{
+    if(R<st||ed<L) return 0;//out of world
+    if(L<=st && ed<=R) return tree[node];
+   ll mid =(st+ed)/2;
+    ll ls=querry(2*node+1,st,mid,L,R);
+   ll rs=querry(2*node+2,mid+1,ed,L,R);
+    return ls+rs;
+
+}
+ll min_sum(ll node,ll st,ll ed,ll L,ll R)
+{
+    if(R<st||ed<L) return LLONG_MAX;//out of world
+  //  if (R < st || ed < L) return LLONG_MIN; // Out of range
+    if(L<=st && ed<=R) return tree[node];
+   ll mid =(st+ed)/2;
+    ll lsm=min_sum(2*node+1,st,mid,L,R);
+   ll rsm=min_sum(2*node+2,mid+1,ed,L,R);
+    return min(lsm,rsm);
+     //return max(lsm,rsm);
+
+
+}
 int main() {
-    // Input array
-    vector<int> input = {1, 3, 5, 7, 9, 11};
-    arr = input;
-    int n = arr.size();
-    tree.resize(4 * n); // Maximum size of segment tree
+    int n, m;
+    cin >> n >> m;
+    arr.resize(n);
+    tree.resize(4 * n);
 
-    // Build the segment tree
-    build(0, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
-    // Querying the sum in the range [1, 3]
-    cout << "Sum of values in range [1, 3]: " << query(0, 0, n - 1, 1, 3) << endl; // Output: 15 (3 + 5 + 7)
 
-    // Updating the value at index 1 to 10
-    update(0, 0, n - 1, 1, 10);
-    cout << "Sum of values in range [1, 3] after update: " << query(0, 0, n - 1, 1, 3) << endl; // Output: 22 (10 + 5 + 7)
+    biuld(0, 0, n - 1);
 
-    return 0;
+    for (int i = 0; i < m; i++) {
+        int t;
+        cin >> t;
+
+        if (t == 1) {
+
+            int idx, val;
+            cin >> idx >> val;
+            update(0, 0, n - 1, idx, val);
+        } else if (t == 2) {
+
+            int l, r;
+            cin >> l >> r;
+            cout <<min_sum(0, 0, n - 1, l, r-1) << endl;
+        }
+    }
+
+
 }
